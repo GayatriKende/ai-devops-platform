64dd3dacf2c9398805bab290c842662d12847d67
Your Name <you@example.com>
2026-03-16 08:52:17 +0000

    Test jenkins trigger

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-     return "AI Devops Platform Running"
+     return "AI Devops Platform Running !!"
 
 @app.route("/health")
 def health():
